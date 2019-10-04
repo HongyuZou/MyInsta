df1103f729027d241f308267ff907a349a4f7f34
HongyuZou <hoz054@ucsd.edu>
2019-10-04 15:13:16 -0700

    start CRUD

--- insta/urls.py
+++ insta/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin 
 from django.urls import include, path
-from insta.views import HelloWorld
+from insta.views import HelloWorld, PostView
 
 urlpatterns = [
     path('', HelloWorld.as_view(), name="helloworld"),  
+    path('posts/', PostView.as_view(), name="posts")
 ]
--- insta/views.py
+++ insta/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from insta.models import Post
 # Create your views here.
 
 class HelloWorld(TemplateView):
     template_name = 'test.html'
 
+class PostView(ListView):
+    model = Post
+    template_name = 'index.html'
+    
